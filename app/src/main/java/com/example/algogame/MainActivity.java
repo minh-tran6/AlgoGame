3b6299d617ee34d5bb60f2c83e0d0910907dc510
toidle <binhboy2000@gmail.com>
2021-03-25 11:14:29 -0500

    secondtry

--- app/src/main/java/com/example/algogame/MainActivity.java
+++ app/src/main/java/com/example/algogame/MainActivity.java
@@ -2,13 +2,32 @@ package com.example.algogame;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button stackgame;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+
+
+
+        //------------------------------------------------------------------------------
+        // STACK GAME MENU
+        stackgame = findViewById(R.id.stackgame);
+        stackgame.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent newStackGame = new Intent(getApplicationContext(),PushandPop_Stack_Tutorial.class);
+                startActivity(newStackGame);
+            }
+        });
+
+        //------------------------------------------------------------------------------
+}
 }
--- app/src/main/java/com/example/algogame/Stack_Game.java
+++ app/src/main/java/com/example/algogame/Stack_Game.java
@@ -3,12 +3,31 @@ package com.example.algogame;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Stack_Game extends AppCompatActivity {
-
+    TextView prompt;
+    TextView scramble;
+    TextView stack;
+    TextView result;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stack__game);
+
+        prompt = findViewById(R.id.prompt);
+        scramble = findViewById(R.id.scramble);
+        stack = findViewById(R.id.stack);
+        result = findViewById(R.id.result);
+
+        //Generating Prompt
+        String bufferPrompt = "BIKECYCLE";
+        prompt.setText(bufferPrompt);
+        //
+
+        //Scramble Text
+        String bufferScramble = "EBKIYCELC";
+        scramble.setText(bufferScramble);
+        //
     }
 }

afc94bad81b9418b8c60f8b537387132af38b83e
minh-tran6 <minh.tran6@mavs.uta.edu>
2021-03-25 10:50:05 -0500

    first try

---
+++ app/src/main/java/com/example/algogame/PushandPop_Stack_Tutorial.java
@@ -0,0 +1,29 @@
+package com.example.algogame;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class PushandPop_Stack_Tutorial extends AppCompatActivity {
+
+    Button start_button_stack;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pushand_pop__stack__tutorial);
+
+        start_button_stack = findViewById(R.id.start_button_stack);
+        start_button_stack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent newGame = new Intent(getApplicationContext(),Stack_Game.class);
+                startActivity(newGame);
+            }
+        });
+    }
+
+
+}
---
+++ app/src/main/java/com/example/algogame/Stack_Game.java
@@ -0,0 +1,14 @@
+package com.example.algogame;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Stack_Game extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stack__game);
+    }
+}

de766da1c2115eca4628966f0c40f13b2acf4590
Pham-Hieu-T <52151504+Pham-Hieu-T@users.noreply.github.com>
2021-03-28 02:21:33 -0500

    by Hieu Pham

--- app/src/main/java/com/example/algogame/MainActivity.java
+++ app/src/main/java/com/example/algogame/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.algogame;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -22,7 +25,9 @@ public class MainActivity extends AppCompatActivity {
 
         //------------------------------------------------------------------------------
         // STACK GAME MENU
+
         stackgame = findViewById(R.id.button_stack);
+        stackgame.setBackgroundColor(Color.BLACK);
         stackgame.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -35,6 +40,7 @@ public class MainActivity extends AppCompatActivity {
         //------------------------------------------------------------------------------
         // QUEUE GAME MENU
         queuegame = findViewById(R.id.button_queue);
+        queuegame.setBackgroundColor(Color.BLACK);
         queuegame.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -47,11 +53,40 @@ public class MainActivity extends AppCompatActivity {
         //------------------------------------------------------------------------------
         // SORT GAME MENU
         sortgame = findViewById(R.id.button_sort);
+        sortgame.setBackgroundColor(Color.BLACK);
         sortgame.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent newSortGame = new Intent(getApplicationContext(),Sort_Game.class);
                 startActivity(newSortGame);
+            }
+        });
+
+        //------------------------------------------------------------------------------
+        //------------------------------------------------------------------------------
+        // EXIT APP
+        exitApp = findViewById(R.id.button_exit);
+        exitApp.setBackgroundColor(Color.BLACK);
+        exitApp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setMessage("Do you want to exit ?");
+                builder.setCancelable(true);
+                builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int i) {
+                        dialog.cancel();
+                    }
+                });
+                builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int i) {
+                        finish();
+                    }
+                });
+                AlertDialog alert = builder.create();
+                alert.show();
             }
         });
 

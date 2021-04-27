be672b6444ebf16ce2031d447368d96b8bdc2071
minh-tran6 <minh.tran6@mavs.uta.edu>
2021-04-27 13:17:20 -0500

    Queue Game Tutorial

--- app/src/main/java/com/example/algogame/QueueandDequeue_Queue_Tutorial.java
+++ app/src/main/java/com/example/algogame/QueueandDequeue_Queue_Tutorial.java
@@ -30,9 +30,10 @@ public class QueueandDequeue_Queue_Tutorial extends AppCompatActivity {
         textInScroll = findViewById(R.id.textInScroll);
         textInScroll.setText("Your  goal  is  to  make  scramble  word  to  become  prompt  word.\n" +
                 "\n\n\n" +
-                "1.  PUSH  :  move  first  letter  of  Scramble  to  the  Stack  until  there  is  no  more  letters  left  in  Scramble.\n\n" +
-                "2.  POP  :  move  the  top  letter  from  the  Stack  to  Result  until  there  is  no  more  letters  left  in  Stack.\n\n" +
-                "3.  RESET  :  restart  the  game  and  try  again  with  Scramble  word.\n\n" +
-                "4.  SUBMIT  :  when  you’re  ready  with  your  RESULT.");
+                "1.  LEFT  :  move  your  Robot  (|)  to  the  left.\n\n" +
+                "2.  RIGHT  :  move  your  Robot  (|)  to  the  right.\n\n" +
+                "3.  DOWN  :  at  the  position  of  Robot  (|) ,  click  this  button  means  you  are  choosing  this  letter  of  Scramble  to  and  move  to  Result.\n\n" +
+                "4.  RESET  :  restart  the  game  and  try  again  with  Scramble  word.\n\n" +
+                "5.  SUBMIT  :  when  you’re  ready  with  your  RESULT.");
     }
 }

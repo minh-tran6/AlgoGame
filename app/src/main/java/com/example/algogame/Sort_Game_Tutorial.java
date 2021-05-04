82aadfb66be65970e1734868d1120d20d61f3bff
Prithvidhar reddy Pudu <PrithviDharpudu@hotmail.com>
2021-05-04 06:49:46 -0500

    made tutorial text clearer

--- app/src/main/java/com/example/algogame/Sort_Game_Tutorial.java
+++ app/src/main/java/com/example/algogame/Sort_Game_Tutorial.java
@@ -31,7 +31,7 @@ public class Sort_Game_Tutorial extends AppCompatActivity {
                 "Assume this is the first step in the outer loop of each sort meaning that the numbers only change position once\n"+
                 "Here are a few refreshers on the 3 sorts in this game\n\n"+
                 "1. Insertion Sort: This sorting simply moves the current number in an array into its appropriate position. It does this by moving back every position until it is the right one. This algorithm starts with the last number.\n\n"+
-                "2. Bubble Sort: This algorithm simply compares the number in front of it and swap if necessary. Starts with the first number\n\n"+
+                "2. Bubble Sort: This algorithm simply compares the number in front of it and swaps if necessary. Starts with the first number\n\n"+
                 "3. Selection Sort: This is similar to insertion sort except that instead of moving back step by step, the number are immediately placed in the right position. Starts with last number\n\n"+
                 "Hope you learn a little about the various sorting algorithms! Have fun! ");
     }
